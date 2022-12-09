f9ba305557adbf6458ae4d81ef51935b62351a69
IlBaia <lorenzo.baiardi@stud.unifi.it>
2022-12-09 16:52:23 +0100

    add circles

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/utility.hpp>
 
+#define TEST100 100
+#define TEST1000 1000
+#define TEST10000 10000
+
+#define MAX_ROWS 500
+#define MAX_COLS 500
+#define MAX_RADIUS 50
+
+#define ALPHA 0.3
+#define BLUE cv::Scalar(178, 34, 34)
+#define RED cv::Scalar(30, 144, 255)
+#define YELLOW cv::Scalar(255, 255, 0)
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    cv::Mat whiteMatrix(200, 200, CV_8UC3, cv::Scalar(255, 255, 255));//Declaring a white matrix
-    cv::Point center(100, 100);//Declaring the center point
-    int radius = 50; //Declaring the radius
-    cv::Scalar line_Color(0, 0, 0);//Color of the circle
-    int thickness = 2;//thickens of the line
-    cv::namedWindow("whiteMatrix");//Declaring a window to show the circle
-    circle(whiteMatrix, center,radius, line_Color, thickness);//Using circle()function to draw the line//
-    imshow("WhiteMatrix", whiteMatrix);//Showing the circle//
-    cv::waitKey(0);//Waiting for Keystroke//
+    std::srand(time(nullptr));
+    cv::Mat matrix(MAX_ROWS, MAX_COLS, CV_8UC3, cv::Scalar(255, 255, 255)); //Declaring a matrix
+    cv::Mat overlay;
+    for(int i=0;i<TEST100;i++){
+        int x = rand()%MAX_ROWS+1;
+        int y = rand()%MAX_COLS+1;
+        int r = rand()%MAX_RADIUS+1;
+        cv::Point center(x, y); //Declaring the center point
+        matrix.copyTo(overlay);
+        switch (rand()%3+1) {
+            case 1:
+                cv::circle(matrix, center, r, BLUE, cv::FILLED);
+                break;
+            case 2:
+                cv::circle(matrix, center, r, RED, cv::FILLED);
+                break;
+            case 3:
+                cv::circle(matrix, center, r, YELLOW, cv::FILLED);
+                break;
+        }
+        cv::addWeighted(overlay, ALPHA, matrix, 1 - ALPHA, 0, matrix);
+    }
+    cv::imshow("TEST", matrix);
+    cv::waitKey(0);
     return 0;
 }
