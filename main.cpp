6dbe6b953f13558a417123827155e14fac69aa26
thomasdelmoro21 <thomas.delmoro@stud.unifi.it>
2022-12-10 13:01:18 +0100

    aggiunto for annidato

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/utility.hpp>
 
+#define TEST10 10
 #define TEST100 100
 #define TEST200 200
 #define TEST1000 1000
@@ -27,23 +28,26 @@ int main() {
     //int sizes[] = {MAX_ROWS, MAX_COLS, MAX_DEPTH};
     //cv::Mat matrix(3, sizes, CV_8UC3, cv::Scalar(255, 255, 255));
     cv::Mat overlay;
-    for(int i=0; i<TEST200; i++){
-        int x = rand() % MAX_ROWS + 1;
-        int y = rand() % MAX_COLS + 1;
-        //int z = rand() % MAX_DEPTH + 1;
-        int r = rand() % MAX_RADIUS + 1;
-        cv::Point center(x, y); //Declaring the center point
-        matrix.copyTo(overlay);
-        switch (rand()%3+1) {
-            case 1:
-                cv::circle(matrix, center, r, BLUE, cv::FILLED, cv::LINE_AA);
-                break;
-            case 2:
-                cv::circle(matrix, center, r, RED, cv::FILLED, cv::LINE_AA);
-                break;
-            case 3:
-                cv::circle(matrix, center, r, YELLOW, cv::FILLED, cv::LINE_AA);
-                break;
+    for(int i=0; i<TEST10; i++){
+#pragma omp parallel for
+        for(int j=0; j<TEST10; j++) {
+            int x = rand() % MAX_ROWS + 1;
+            int y = rand() % MAX_COLS + 1;
+            //int z = rand() % MAX_DEPTH + 1;
+            int r = rand() % MAX_RADIUS + 1;
+            cv::Point center(x, y); //Declaring the center point
+            matrix.copyTo(overlay);
+            switch (rand()%3+1) {
+                case 1:
+                    cv::circle(matrix, center, r, BLUE, cv::FILLED, cv::LINE_AA);
+                    break;
+                case 2:
+                    cv::circle(matrix, center, r, RED, cv::FILLED, cv::LINE_AA);
+                    break;
+                case 3:
+                    cv::circle(matrix, center, r, YELLOW, cv::FILLED, cv::LINE_AA);
+                    break;
+            }
         }
         cv::addWeighted(overlay, ALPHA, matrix, 1 - ALPHA, 0, matrix);
     }
