4d893462d0e85e1a0feea9efcc6c04628aea2853
loreb <lorebaia@gmail.com>
2023-03-17 00:36:23 +0100

    completed project seq e par TODO: resolve white background

--- main.cpp
+++ main.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
-#include <chrono>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/utility.hpp>
 #include <vector>
-
-#ifdef _OPENMP
-
 #include <omp.h>
-
-#endif
 
 #define WIDTH 500
 #define HEIGHT 500
 #define TRANSPARENT cv::Scalar(255, 255, 255, 0)
 #define ALPHA 0.3
-#define N_PLANES_100 100
-#define N_PLANES_1000 1000
-#define N_PLANES_5000 5000
 
 #define MAX_RADIUS 70
 #define MIN_RADIUS 10
-#define N_CIRCLES 2000
+
+#define N_PLANES_1 2
+#define N_PLANES_2 20
+#define N_CIRCLES 2
 
 struct Circle {
     cv::Scalar color;
     cv::Point center;
-    int r = 0;
+    int r;
 };
 
-void generate_circles(Circle circles[], int nCircle, int idPlane) {
-    std::srand(idPlane);
-    for (int i = 0; i < nCircle; i++) {
-        int x = std::rand() % HEIGHT + 1;
-        int y = std::rand() % WIDTH + 1;
-        cv::Point center(x, y);
+void generate_circles(Circle circles[], unsigned long long n) {
+    std::srand(0);
+    for (int i = 0; i < n; i++) {
         cv::Scalar color(std::rand() % 256, std::rand() % 256, std::rand() % 256);
+        cv::Point center(std::rand() % HEIGHT + 1, std::rand() % WIDTH + 1);
         int r = std::rand() % (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS + 1;
         circles[i] = {color, center, r};
     }
 }
 
-double renderer_seq(int nPlanes, int nCircles) {
+
+
+double renderer_seq(Circle circles[], int nPlanes, int nCircles) {
     printf("RENDERER SEQUENTIAL\n");
     cv::Mat result(HEIGHT, WIDTH, CV_8UC4, TRANSPARENT);
 
@@ -53,10 +46,10 @@ double renderer_seq(int nPlanes, int nCircles) {
         cv::Mat plane;
         result.copyTo(plane);
         printf("PLANE: %d\n", i + 1);
-        Circle circles[N_CIRCLES];
-        generate_circles(circles, N_CIRCLES, i);
-        for (int j = 0; j < nCircles; j++)
-            cv::circle(plane, circles[j].center, circles[j].r, circles[j].color, cv::FILLED, cv::LINE_AA);
+        for (int j = 0; j < nCircles; j++) {
+            Circle circle = circles[i*nCircles+j];
+            cv::circle(plane, circle.center, circle.r, circle.color, cv::FILLED, cv::LINE_AA);
+        }
         cv::addWeighted(plane, ALPHA, result, 1 - ALPHA, 0, result);
     }
 
@@ -68,7 +61,7 @@ double renderer_seq(int nPlanes, int nCircles) {
     return time;
 }
 
-double renderer_par(int nPlanes, int nCircles) {
+double renderer_par(Circle circles[], int nPlanes, int nCircles) {
     printf("RENDERER PARALLEL\n");
     cv::Mat result(HEIGHT, WIDTH, CV_8UC4, TRANSPARENT);
     std::vector<cv::Mat> planes;
@@ -77,15 +70,16 @@ double renderer_par(int nPlanes, int nCircles) {
 
     double start = omp_get_wtime();
 
-#pragma omp parallel default(none) shared(planes, nCircles)
+#pragma omp parallel default(none) shared(planes, circles, nCircles)
 #pragma omp for
     for (int i = 0; i < planes.size(); i++) {
-        Circle circles[N_CIRCLES];
-        generate_circles(circles, N_CIRCLES, i);
-        for (int j = 0; j < nCircles; j++)
-            cv::circle(planes[i], circles[j].center, circles[j].r, circles[j].color, cv::FILLED, cv::LINE_AA);
         printf("PLANE: %d\n", i + 1);
+        for (int j = 0; j < nCircles; j++) {
+            Circle circle = circles[i*nCircles+j];
+            cv::circle(planes[i], circle.center, circle.r, circle.color, cv::FILLED, cv::LINE_AA);
+        }
     }
+#pragma omp barrier
 
     for (const auto &plane: planes)
         cv::addWeighted(plane, ALPHA, result, 1 - ALPHA, 0, result);
@@ -98,13 +92,25 @@ double renderer_par(int nPlanes, int nCircles) {
 
 int main() {
 #ifdef _OPENMP
-    printf("Core/Threads: %d\n", omp_get_num_procs());
+    int threads = omp_get_num_procs();
+    printf("Core/Threads: %d\n", threads);
+    omp_set_dynamic(0);
 #endif
-    std::vector<int> test_planes = {N_PLANES_100, N_PLANES_1000};
+    std::vector<double> sequential_times;
+    std::vector<double> parallel_times;
+
+    std::vector<int> test_planes = {N_PLANES_1 * threads, N_PLANES_2 * threads};
 
     for (int test: test_planes) {
-        renderer_seq(test, N_CIRCLES);
-        renderer_par(test, N_CIRCLES);
+        const unsigned long long n = test*N_CIRCLES;
+        Circle circles[n];
+        generate_circles(circles, n);
+        double t1 = renderer_seq(circles, test, N_CIRCLES);
+        double t2 = renderer_par(circles, test, N_CIRCLES);
+        sequential_times.push_back(t1);
+        parallel_times.push_back(t2);
     }
+
+
 }
 
